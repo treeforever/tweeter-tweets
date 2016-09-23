92430a7ac86ce0ecd1b9f20d658db14f507229f7
treeforever <treeforever7@gmail.com>
2016-09-23 18:58:11 +0000

    edited README.md

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('textarea').on('keyup', function() {
+  $('textarea').on('keypress', function() {
     var wordLength = $(this).val().length;
     var wordCount = 140 - wordLength;
     var counter = $(this).siblings('.counter');
